fd92f20c2e785ad1a9c82d1bd168ecddf5bb7993
mengdieZzz66 <1250207285@qq.com>
2022-01-22 22:45:52 +0800

    创建新工程

---
+++ src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>login</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.login();
+  },
+  methods: {
+    login() {
+      debugger
+      
+      console.log("login-----------------------");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
